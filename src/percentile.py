d2c9a466b9a4f904c9c6eeed016f9efeb86262a1
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2022-01-02 17:29:25 +0530

    percentile

---
+++ src/percentile.py
@@ -0,0 +1,8 @@
+# Mindula Dilthushan
+
+import numpy as pe
+
+ages=pe.array([5,31,43,48,50,41,7,11,15,39,80,82,32,2,8,6,25,36,27,61,31])
+
+x=pe.percentile(ages,90) # In this function basic model is percentile(array,percentage)
+print(x)
