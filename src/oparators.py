944a951f34aba37b964550f38af3fabc94b7ce01
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2022-01-27 09:35:17 +0530

    operators

---
+++ src/oparators.py
@@ -0,0 +1,20 @@
+a= not True
+b= ~ False
+print(a+b)
+
+x=10
+y=80
+
+# we can write this as below
+
+x,y=10,90
+
+min=x if x<y else y
+print(min)
+
+
+print(7//3)
+print(-7//3)
+
+print(any([False,True,False,False]))
+print(all([True,True,True,False]))
